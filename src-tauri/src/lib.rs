b6218fccfe4640e7723f911ce94540207ee6df2d
sexyfeifan <zhoufeifan@gmail.com>
2026-08-05 20:37:29 +0800

    fix: update updater pubkey to v3 keypair

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -55,7 +55,7 @@ pub fn run() {
         .plugin(tauri_plugin_clipboard_manager::init())
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_global_shortcut::Builder::new().build())
-        .plugin(tauri_plugin_updater::Builder::new().pubkey("RWRTY19e5anhI4Hh+xIvZndbtrPBkapHzKvW6mTskG+h5soOQY1jGObq").build())
+        .plugin(tauri_plugin_updater::Builder::new().pubkey("RWSMQDXV/UKkliWsab83mjjMVZhv6rM3QpsFf3wZoiXip0vLS3h3YMsA").build())
         .invoke_handler(tauri::generate_handler![
             commands::get_history,
             commands::get_history_page,
